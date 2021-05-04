95304d7bb7d6b535c47ea983390c3505f8cd1298
yuliamed <yulinwo.17@gmail.com>
2021-05-04 18:55:35 +0300

    add spinners to the search xml file

--- app/src/main/java/com/example/yulicar/CentralFragment.java
+++ app/src/main/java/com/example/yulicar/CentralFragment.java
@@ -23,6 +23,7 @@ public class CentralFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String [] directions = {"Minsk - Mogilev", "Mogilev - Minsk"};
 
     public CentralFragment () {
         // Required empty public constructor
@@ -58,7 +59,13 @@ public class CentralFragment extends Fragment {
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState) {
+
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate (R.layout.fragment_search, container, false);
+
+
     }
 }
