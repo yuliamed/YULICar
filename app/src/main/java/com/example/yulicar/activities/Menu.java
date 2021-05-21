8ca421a90174dbcbfff3ceec39a5966ab728198c
yuliamed <yulinwo.17@gmail.com>
2021-05-22 01:48:29 +0300

    fix the way in switching pages

--- app/src/main/java/com/example/yulicar/activities/Menu.java
+++ app/src/main/java/com/example/yulicar/activities/Menu.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.yulicar.R;
 import com.example.yulicar.fragments.CentralFragment;
@@ -16,34 +17,44 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Menu extends AppCompatActivity {
 
-
+    final Fragment search = new CentralFragment ();
+    final Fragment user = new UserFragment ();
+    final Fragment help = new HelpFragment();
+    Fragment active = search;
+    final FragmentManager fm = getSupportFragmentManager ();
     @Override
     protected void onCreate (@Nullable Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.central_activity);
 
-        BottomNavigationView bottomNavigationView= findViewById (R.id.navigation_view);
-        bottomNavigationView.setOnNavigationItemSelectedListener (navigationItemSelectedListener);
-        getSupportFragmentManager ().beginTransaction ().replace (R.id.fragment_conteiner, new CentralFragment ()).commit ();
+        BottomNavigationView navigation = findViewById (R.id.navigation_view);
+        navigation.setOnNavigationItemSelectedListener (navigationItemSelectedListener);
+        //fm.beginTransaction ().add (R.id.fragment_conteiner, help).hide(help).commit ();
+        //fm.beginTransaction ().add (R.id.fragment_conteiner, user).hide(user).commit ();
+        fm.beginTransaction ().add (R.id.fragment_conteiner, search).commit ();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener () {
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener () {
         @Override
         public boolean onNavigationItemSelected (@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
             switch (item.getItemId ()) {
                 case R.id.helpFragment:
-                    selectedFragment = new HelpFragment ();
-                    break;
+                    //fm.beginTransaction ().hide (item.getActionView ).show (help).commit ();
+                    active = help;
+                    fm.beginTransaction ().replace (R.id.fragment_conteiner, active).commit ();
+                    return true;
                 case R.id.searchFragment:
-                    selectedFragment = new CentralFragment ();
-                    break;
+                    //fm.beginTransaction ().hide (active).show (search).commit ();
+                    active = search;
+                    fm.beginTransaction ().replace (R.id.fragment_conteiner, active).commit ();
+                    return true;
                 case R.id.userFragment:
-                    selectedFragment = new UserFragment ();
-                    break;
-
+                    //fm.beginTransaction ().hide (active).show (user).commit ();
+                    active = user;
+                    fm.beginTransaction ().replace (R.id.fragment_conteiner, active).commit ();
+                    return true;
             }
-            getSupportFragmentManager ().beginTransaction ().replace (R.id.fragment_conteiner, selectedFragment).commit ();
             return false;
         }
     };
