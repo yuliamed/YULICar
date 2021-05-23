c59998c1cb621e28825b699bf9dac60fc6828194
yuliamed <yulinwo.17@gmail.com>
2021-05-23 14:27:37 +0300

    fix bag with booking the same trip

--- app/src/main/java/com/example/yulicar/fragments/OneTripFragment.java
+++ app/src/main/java/com/example/yulicar/fragments/OneTripFragment.java
@@ -1,5 +1,6 @@
 package com.example.yulicar.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,41 +16,44 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.yulicar.R;
+import com.example.yulicar.activities.MainActivity;
 import com.example.yulicar.db.DBManeger;
 import com.example.yulicar.db.entities.Trip;
+import com.example.yulicar.db.entities.User;
 
 import java.util.Calendar;
+import java.util.List;
 
 
 public class OneTripFragment extends Fragment {
-    TextView time, car,carNumber,numbOfSeats;
+    TextView time, car, carNumber, numbOfSeats;
     Button priceBook;
     private DBManeger dbManeger;
-
+    private Long tripId;
+    private Trip trip;
 
     @Nullable
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View v = inflater.inflate (R.layout.fragment_one_trip, container, false);
         Log.d ("DB-TEST-OneTrip", "i'm here");
         dbManeger = new DBManeger (getContext ());
-        Long tripId;
-        Trip trip;
+
         time = (TextView) v.findViewById (R.id.time);
         car = (TextView) v.findViewById (R.id.car);
         carNumber = (TextView) v.findViewById (R.id.carNumber);
         numbOfSeats = (TextView) v.findViewById (R.id.numbOfSeats);
 
         priceBook = (Button) v.findViewById (R.id.price_book);
-        String timeS, carS,carNumberS,numbOfSeatsS, priceS;
-        Bundle bundle = getArguments();
+        Bundle bundle = getArguments ();
         if (bundle == null) {
             return v;
         } else {
             tripId = bundle.getLong ("tripId");
             trip = dbManeger.dao.getTripById (tripId);
         }
-        String hourS = changeFormatOfNumbString (trip.getDate ().get(Calendar.HOUR_OF_DAY));
+        String hourS = changeFormatOfNumbString (trip.getDate ().get (Calendar.HOUR_OF_DAY));
         String minuteS = changeFormatOfNumbString (trip.getDate ().get (Calendar.MINUTE));
         time.setText (hourS + ":" + minuteS);
         car.setText (trip.getCarName ());
@@ -57,28 +61,42 @@ public class OneTripFragment extends Fragment {
         numbOfSeats.setText (trip.getNumbOfSeats () + " мест");
         priceBook.setText (trip.getPrice () + " BYR");
 
+        if (hasUserTrip ()) {
+            priceBook.setText ("уже заказано");
+            priceBook.setTextSize (18);
+            return v;
+        }
 
         priceBook.setOnClickListener (new View.OnClickListener () {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick (View v) {
-
                 BookTripFragment bookTripFragment = new BookTripFragment ();
                 Bundle bundle = new Bundle ();
-                bundle.putLong("tripId", tripId/*info from spinner*/);
-                Log.d("DB-TEST-selectedTrip", String.valueOf (tripId) );
+                bundle.putLong ("tripId", tripId/*info from spinner*/);
+                Log.d ("DB-TEST-selectedTrip", String.valueOf (tripId));
                 bookTripFragment.setArguments (bundle);
-                FragmentManager fragmentManager=getActivity().getSupportFragmentManager ();
-                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                FragmentManager fragmentManager = getActivity ().getSupportFragmentManager ();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction ();
                 fragmentTransaction.replace (R.id.fragment_conteiner, bookTripFragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                fragmentTransaction.addToBackStack (null);
+                fragmentTransaction.commit ();
 
             }
         });
         return v;
     }
 
-    public static String changeFormatOfNumbString(int value) {
+    private boolean hasUserTrip () {
+        List<User> users = dbManeger.dao.selectUsersByTripId (tripId);
+        int savedNumber = MainActivity.mSettings.getInt (MainActivity.APP_PREFERENCES_USERID, 0);
+        for (User u : users) {
+            if (u.getPhNumber ().equals (savedNumber)) return true;
+        }
+        return false;
+    }
+
+    public static String changeFormatOfNumbString (int value) {
         if (value < 10) {
             return "0" + String.valueOf (value);
         } else return String.valueOf (value);
